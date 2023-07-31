d77c73614f87eded07e27e7973f428f1c36d93ea
Min <als904204@naver.com>
2023-07-31 23:12:57 +0900

    security h2 images error fix

--- src/main/java/com/example/oauth/util/security/SecurityConfig.java
+++ src/main/java/com/example/oauth/util/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -30,6 +31,9 @@ public class SecurityConfig {
                 .defaultSuccessUrl("/")
                 .userInfoEndpoint(point -> point
                     .userService(customOAuth2UserService))
+            )
+            .headers(headers->headers
+                .frameOptions(FrameOptionsConfig::disable)
             )
             .csrf(csrf -> csrf
                 .ignoringRequestMatchers(toH2Console())
